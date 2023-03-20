57f07fa4223ef9857ee7af68274d23e86cdbb9eb
Amrish Parmar <amrish.parmar@beauhurst.com>
2023-03-20 15:03:30 +0000

    Drop `get_aws_credentials` helper function

--- env_templating/aws.py
+++ env_templating/aws.py
@@ -3,13 +3,6 @@ import json
 import boto3
 
 
-def get_aws_credentials(session: boto3.session.Session) -> tuple[str, str]:
-    if session is None:
-        session = boto3.Session()
-    credentials = session.get_credentials()
-    return credentials.access_key, credentials.secret_key
-
-
 def get_aws_secret(session: boto3.session.Session, secret_name: str) -> dict:
     client = session.client("secretsmanager")
     response = client.get_secret_value(SecretId=secret_name)
